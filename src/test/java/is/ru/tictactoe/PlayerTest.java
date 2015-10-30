47cfe44d9ca02c0db0ce54cea6663e9879ae656f
laruskonrad <laruskonrad@hotmail.com>
2015-10-30 11:45:07 +0000

    Added coordinates and wrote and tested Player class

--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -1,8 +1,44 @@
 package is.ru.tictactoe;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class PlayerTest {
 
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeTest");
+    }
+
+    @Test
+    public void getNameTest() {
+    	Player P1 = new Player("Steven");
+
+    	assertEquals("Steven", P1.getName());
+    }
+
+	@Test
+    public void getNumberOfWinsTest() {
+    	Player P1 = new Player("Steven");
+
+    	//Player starts with zero wins
+    	assertEquals(0, P1.getNumberOfWins());
+    }
+
+    @Test
+    public void playerWinsTest() {
+    	Player P1 = new Player("Steven");
+    	P1.playerWon();
+
+    	assertEquals(1, P1.getNumberOfWins());
+    }
+
+    @Test
+    public void playerMultipleWinsTest() {
+    	Player P1 = new Player("Steven");
+    	P1.playerWon();
+    	P1.playerWon();
+    	P1.playerWon();
+
+    	assertEquals(3, P1.getNumberOfWins());
     }
 }
