71fb815d0d1c5dd8a01fb0dc663805fdd6f5cf0b
laruskonrad <laruskonrad@hotmail.com>
2015-10-31 15:24:02 +0000

    First steps in computerPlayer

--- src/main/java/is/ru/tictactoe/ComputerPlayer.java
+++ src/main/java/is/ru/tictactoe/ComputerPlayer.java
@@ -1,10 +1,57 @@
 package is.ru.tictactoe;
 
-public class ComputerPlayer {
+public class ComputerPlayer extends Player {
 
-	public static char[][] setBoard() {
-		char board[][]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-		return board;
+	public ComputerPlayer(String name, char marker) {
+		super(name, marker);
 	}
 
+	public Coordinates getMove(char[][] board) {
+		Coordinates invalidMove = new Coordinates(3,3);
+
+		if(checkHorizontal(board) != invalidMove) {
+			return checkHorizontal(board);
+		}
+
+		return invalidMove;
+	}
+
+	public Coordinates checkHorizontal(char[][] board) {
+		Coordinates move;
+		char mark;
+		if(super.marker == 'x') {
+			mark = 'o';
+		}
+		else {
+			mark = 'x';
+		}
+
+		//Check if computer can win on 3 row
+		for(int i = 0; i < 2; i++) {
+			if(board[i][0] == board[i][1] && board[i][0] == mark && board[i][2] == ' ') {
+				move = new Coordinates(i,2);
+				return move;
+			}
+		}
+
+		//Check if computer can win on 2 row
+		for(int i = 0; i < 2; i++) {
+			if(board[i][0] == board[i][2] && board[i][0] == mark && board[i][1] == ' ') {
+				move = new Coordinates(i,1);
+				return move;
+			}
+		}
+
+		//Check if computer can win on 1 row
+		for(int i = 0; i < 2; i++) {
+			if(board[i][1] == board[i][2] && board[i][1] == mark && board[i][0] == ' ') {
+				move = new Coordinates(0,i);
+				return move;
+			}
+		}
+
+		move = new Coordinates(3,3);
+
+		return move;
+	}
 }
--- src/test/java/is/ru/tictactoe/ComputerPlayerTest.java
+++ src/test/java/is/ru/tictactoe/ComputerPlayerTest.java
@@ -1,9 +1,42 @@
 package is.ru.tictactoe;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class ComputerPlayerTest {
 
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeTest");
     }
 
+    @Test
+    public void getNameTest() {
+    	ComputerPlayer P1 = new ComputerPlayer("HAL9000", 'O');
+
+    	assertEquals("HAL9000", P1.getName());
+    }
+
+    @Test
+    public void checkHorizontalFailureTest() {
+        ComputerPlayer P1 = new ComputerPlayer("HAL9000", 'X');
+        Coordinates coord = new Coordinates(3,3);
+        char[][] board = new char[3][3];
+
+        assertEquals(true, coord.equals(P1.checkHorizontal(board)));
+    }
+
+    @Test
+    public void checkHorizontalTest() {
+        ComputerPlayer P1 = new ComputerPlayer("HAL9000", 'X');
+        Coordinates coord = new Coordinates(0,0);
+        Coordinates coordH = new Coordinates(2,2);
+        Coordinates coord2 = new Coordinates(0,1);
+        Coordinates coord3 = new Coordinates(0,2);
+        TicTacToe game = new TicTacToe();
+        game.playersTurn(coord);
+        game.playersTurn(coordH); //To get o turn
+        game.playersTurn(coord2);
+
+        assertEquals(true, coord3.equals(P1.checkHorizontal(game.getBoard())));
+    }
 }
